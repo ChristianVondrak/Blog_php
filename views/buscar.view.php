69b0e0afeef99fad706acb635712d36a3ec48d1d
Christian Vondrak <christian20499@gmail.com>
2022-09-02 16:24:22 -0400

    Se agrego funcionalidad a barra de busqueda

---
+++ views/buscar.view.php
@@ -0,0 +1,29 @@
+<?php
+require 'header.php';
+?>
+
+<div class="contenedor">
+    
+    <?php foreach($resultados as $p): ?>
+    <div class="post">
+        <article>
+            <h2 class="titulo"><a href="single.php?id=<?php echo $p['ID']; ?>"><?php echo $p['titulo']; ?></a></h2>
+            <p class="fecha"><?php echo fecha($p['fecha']); ?></p>
+            <div class="thumb">
+                <a href="single.php?id=<?php echo $p['ID']; ?>">
+                    <img src="<?php echo RUTA; ?>/img/<?php echo $p['thumb']; ?>" alt="">
+                </a>
+            </div>
+            <p class="extracto"><?php echo $p['extracto']; ?></p>
+            <a href="single.php?id=<?php echo $p['ID'] ?>;" class="continuar">Leer mas</a>
+        </article>
+    </div>
+    <?php endforeach ?>
+
+    <?php require 'paginacion.php'; ?>
+
+</div>
+    <?php require 'footer.php';?>
+</body>
+
+</html>
